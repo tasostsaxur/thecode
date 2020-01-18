c200a8dc945750e5d99984d84eed877f9fe40aee
Tassos Tsachourides <tasostsaxur@yahoo.com>
2020-01-18 20:53:47 +0100

    Create nodejsdocker.groovy

---
+++ nodejsdocker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS Docker example') {
+    scm {
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {							// we don't have  the 'shell command' to do 'npm install' anymore
+        dockerBuildAndPublish {     // cloudbees plugin command (v1.2+ required by JobDSL)
+            repositoryName('tasostsaxur/docker-nodejs-demo')
+            tag('${GIT_REVISION,length=9}') 	// ! every new container version (when we commit on github) will be the same as the git version
+            registryCredentials('dockerhub')	// ! will create credentials with dockerhub as ID later
+            forcePull(false)					// from the job dsl API page for docker
+            forceTag(false)						// from the job dsl API page for docker
+            createFingerprints(false)			// from the job dsl API page for docker
+            skipDecorate()						// from the job dsl API page for docker
+        }
+    }
+}
