09a9ade34397282e438ce692abb7fc211d1c8f17
Tassos Tsachourides <tasostsaxur@yahoo.com>
2020-01-18 21:06:46 +0100

    Update nodejsdocker.groovy

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -1,8 +1,8 @@
-job('NodeJS Docker example') {
+job('NodeJS Docker example tassos') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('tasosostsaxue@yahoo.com')
         }
     }
     triggers {
